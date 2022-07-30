6ca9f6f1b9171b9e70950eeaf70291171a3a1ec7
idanmashiach <idanm@r-stealth.com>
2022-07-30 13:45:00 +0300

    addded the deep learning

--- ui/src/deepLearning/model.js
+++ ui/src/deepLearning/model.js
@@ -2,9 +2,18 @@
 import * as tf from '@tensorflow/tfjs';
 
 export default class OurModel {
-    static predict() {
-        const input = tf.tensor([[1, 2], [3, 4]]);
-        const model = await tf.loadLayersModel('http://locahost:3000/model.json');
+    static predict(political_tendancy, economical_tendancy, social_tendancy, religious_tendancy, environment_friendly) {
+        const tf = require("@tensorflow/tfjs");
+        const tfn = require("@tensorflow/tfjs-node");
+        const handler = tfn.io.fileSystem("./tfjs/model.json");
+        const model = await tf.loadLayersModel(handler);
+
+
+        const input = tf.tensor([[political_tendancy, economical_tendancy, social_tendancy, religious_tendancy, 
+            environment_friendly]]);
+        // const model = await tf.loadLayersModel('http://locahost:3000/model.json');
+
         const output = model.predict(input);
+        const selected = output.argMax();
     }
 }
